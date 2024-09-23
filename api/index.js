4fc4744f1e8ff932ba8b8972dfa908b9101621f4
Arjit <arjitloh223@gmail.com>
2024-09-23 16:10:18 +0545

    signup page start

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 
 import dotenv from 'dotenv';
 import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -13,14 +14,20 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000 !!!');
 });
 
 app.use('/api/user',userRouter)
-app.get('/hello', (req, res) => {
-    res.json({
-        message: 'Hello World',
+app.use('/api/auth',authRouter)
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode: statusCode,
+        message
     });
-});
+})
