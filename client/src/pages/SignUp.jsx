498aea99dc83d9cbd58eb6712c0732110f4837b8
Arjit <arjitloh223@gmail.com>
2024-09-24 12:24:15 +0545

    signup complete

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
@@ -8,7 +9,17 @@ export default function SignUp() {
         <input type='text' placeholder='Username' className='border p-3 rounded-lg' id='username'/>
         <input type='email' placeholder='E-mail' className='border p-3 rounded-lg' id='email'/>
         <input type='password' placeholder='Password' className='border p-3 rounded-lg' id='password'/>
+        <button className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95
+        disabled: opacity-80'>Signup</button>
+
       </form>
+      <div className='flex gap-2 mt-5'>
+      <p>Have an account ?</p>
+      <Link to={'/sign-in'} className='text-red'><span className='text-blue-700'>
+      Sign in</span></Link>
+        
+      </div>
+     
     </div>
   )
 }
